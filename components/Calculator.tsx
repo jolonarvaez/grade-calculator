babdb94a6a24a3abb51713ba525199276e92eef7
Jolo Narvaez <61716695+jolonarvaez@users.noreply.github.com>
2023-04-30 21:46:51 +0800

    added school color and transitions

---
+++ components/Calculator.tsx
@@ -0,0 +1,72 @@
+import React, { use, useState } from "react";
+import Row from "./Row";
+import { BsFillPlusCircleFill } from "react-icons/bs";
+
+export default function Calculator() {
+  const [school, setSchool] = useState("DLSU");
+
+  const changeSchool = (string: any) => {
+    setSchool(string);
+  };
+
+  return (
+    <div className="container mx-auto max-w-2xl mt-4">
+      <div className="w-1/2 mx-auto">
+        <div className="text-center font-bold flex flex-row text-lg">
+          <div
+            className={`transition duration-200 w-1/3 py-1 rounded-t-lg text-white ${
+              school === "DLSU" ? "bg-dlsu-green" : ""
+            } ${school === "UP" ? "text-dlsu-green" : ""} ${
+              school === "ADMU" ? "text-dlsu-green" : ""
+            }`}
+          >
+            <button onClick={() => changeSchool("DLSU")}>DLSU</button>
+          </div>
+          <div className={`transition duration-200 w-1/3 py-1 rounded-t-lg text-white ${
+              school === "DLSU" ? "text-up-maroon" : ""
+            } ${school === "UP" ? "bg-up-maroon" : ""} ${
+              school === "ADMU" ? "text-up-maroon" : ""
+            }`}>
+            <button onClick={() => changeSchool("UP")}>UP</button>
+          </div>
+          <div className={`transition duration-200 w-1/3 py-1 rounded-t-lg text-white ${
+              school === "DLSU" ? "text-admu-blue" : ""
+            } ${school === "UP" ? "text-admu-blue" : ""} ${
+              school === "ADMU" ? "bg-admu-blue" : ""
+            }`}>
+            <button onClick={() => changeSchool("ADMU")}>ADMU</button>
+          </div>
+        </div>
+      </div>
+
+      <div className="text-center rounded-lg mx-4 shadow-lg">
+        <div></div>
+        <div
+          className={`transition duration-200 flex flex-row text-white rounded-t-lg py-2.5 ${
+            school === "DLSU" ? "bg-dlsu-green" : ""
+          } ${school === "UP" ? "bg-up-maroon" : ""}${
+            school === "ADMU" ? "bg-admu-blue" : ""
+          }`}
+        >
+          <div className="w-1/3">Course</div>
+          <div className="w-1/3">Units</div>
+          <div className="w-1/3">Grade</div>
+        </div>
+        <div
+          className={`transition duration-200 border-2 rounded-b-lg ${
+            school === "DLSU" ? "border-dlsu-green" : ""
+          } ${school === "UP" ? "border-up-maroon" : ""}${
+            school === "ADMU" ? "border-admu-blue" : ""
+          }`}
+        >
+          <Row />
+          <Row />
+          <Row />
+          <div className="add-btn">
+            <BsFillPlusCircleFill />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
