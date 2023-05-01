e5e241a779a5fdb7311e0f936ad0a7e4134544ee
Jolo Narvaez <61716695+jolonarvaez@users.noreply.github.com>
2023-05-01 12:52:08 +0800

    added add row functionality

--- components/Calculator.tsx
+++ components/Calculator.tsx
@@ -4,13 +4,19 @@ import { BsFillPlusCircleFill } from "react-icons/bs";
 
 export default function Calculator() {
   const [school, setSchool] = useState("DLSU");
+  const [tableData, setTableData] = useState<React.ReactNode[]>([]);
 
   const changeSchool = (string: any) => {
     setSchool(string);
   };
 
+  const addRow = () => {
+    const newDivs = [...tableData, <div key={tableData.length}><Row /></div>];
+    setTableData(newDivs);
+  }
+
   return (
-    <div className="container mx-auto max-w-2xl mt-4">
+    <div className="mx-auto max-w-2xl mt-4 h-80">
       <div className="w-1/2 mx-auto">
         <div className="text-center font-bold flex flex-row text-lg">
           <div
@@ -35,7 +41,6 @@ export default function Calculator() {
       </div>
 
       <div className="text-center rounded-lg mx-4 shadow-lg">
-        <div></div>
         <div
           className={`transition duration-200 flex flex-row text-white rounded-t-lg py-2.5 ${
             school === "DLSU" ? "bg-dlsu-green" : ""
@@ -48,17 +53,23 @@ export default function Calculator() {
           <div className="w-1/3">Grade</div>
         </div>
         <div
-          className={`transition duration-200 border-2 rounded-b-lg ${
+          className={`transition duration-200 border-2 rounded-b-lg min-h-80 ${
             school === "DLSU" ? "border-dlsu-green" : ""
           } ${school === "UP" ? "border-up-maroon" : ""}${
             school === "ADMU" ? "border-admu-blue" : ""
           }`}
         >
-          <Row />
-          <Row />
-          <Row />
-          <div className="add-btn">
-            <BsFillPlusCircleFill />
+          <div>
+            {tableData.map((div) => div)}
+          </div>
+          <div className={`text-3xl my-2 text-dlsu-green ${
+            school === "DLSU" ? "text-dlsu-green" : ""
+          } ${school === "UP" ? "text-up-maroon" : ""}${
+            school === "ADMU" ? "text-admu-blue" : ""
+          }`}>
+            <button onClick={addRow}>
+              <BsFillPlusCircleFill />
+            </button>
           </div>
         </div>
       </div>
--- components/Row.tsx
+++ components/Row.tsx
@@ -4,17 +4,13 @@ import GradeSelect from "./GradeSelect";
 export default function Row() {
   return (
     <div className="flex py-2.5 border-b my-1">
-        <div className="w-1/3">
-        <input
-          type="text" 
-        /> 
-        </div>
-        <div className="w-1/3">
-            Test
-        </div>
-        <div className="w-1/3">
-            <GradeSelect />
-        </div>
+      <div className="w-1/3">
+        <input className="placeholder:text-sm palceholder:text-red-400" type="text" placeholder="Enter Course Name" />
+      </div>
+      <div className="w-1/3">Test</div>
+      <div className="w-1/3">
+        <GradeSelect />
+      </div>
     </div>
   );
 }
